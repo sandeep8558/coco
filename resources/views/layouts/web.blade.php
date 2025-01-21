52cbd62aee6cc7f0e1857b50ed7951b7bf99c4da
leenaa-28 <leenaadam28@gmail.com>
2025-01-21 22:47:59 +0530

    bootstrap installed

--- resources/views/layouts/web.blade.php
+++ resources/views/layouts/web.blade.php
@@ -3,7 +3,9 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  
     @yield('head')
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
 <body>
 
--- resources/views/layouts/web/footer.blade.php
+++ resources/views/layouts/web/footer.blade.php
@@ -1,1 +1,68 @@
-footer
+<!-- Footer -->
+<!-- <div class="container-fluid text-center text-lg-start text-muted" style="background-color: rgba(238, 14, 14, 0.05);"> -->
+<div class="container-fluid bg-light p-3">
+  <!-- Section: Links  -->
+  <section class="">
+    <div class="container text-center text-md-start mt-5">
+      <!-- Grid row -->
+      <div class="row mt-3">
+        <!-- Grid column -->
+        <div class="col-md-3 col-lg-4 col-xl-3 mx-auto mb-4">
+          <!-- Content -->
+          <h6 class="text-uppercase fw-bold mb-4">
+          Coco School
+          </h6>
+          <p>
+          56RC+2MW, Dattanagar, Ambernath, Badlapur, Maharashtra 421505
+          </p>
+        </div>
+        <!-- Grid column -->
+
+        <!-- Grid column -->
+        <div class="col-md-2 col-lg-2 col-xl-2 mx-auto mb-4">
+          <!-- Links -->
+          <h6 class="text-uppercase fw-bold mb-4">
+          Useful links
+          </h6>
+          <ul class="list-unstyled">
+              <li><a href="#" class="text-decoration-none text-dark">Home</a></li>
+              <li><a href="#" class="text-decoration-none text-dark">About</a></li>
+              <li><a href="#" class="text-decoration-none text-dark">Gallery</a></li>
+              <li><a href="#" class="text-decoration-none text-dark">Admission</a></li>
+            </ul>
+          <!-- <p>
+            <a href="#!" class="text-reset">Home</a>
+          </p> -->
+         
+        </div>
+        <!-- Grid column -->
+
+    
+
+        <!-- Grid column -->
+        <div class="col-md-4 col-lg-3 col-xl-3 mx-auto mb-md-0 mb-4">
+          <!-- Links -->
+          <h6 class="text-uppercase fw-bold mb-4">Contact</h6>
+          <p><i class="fa fa-brands fa-facebook fa-lg" style="color: #FFD43B;"></i> New York, NY 10012, US</p>
+          <p>
+            <i class="fas fa-envelope me-3"></i>
+            info@example.com
+          </p>
+          <p><i class="fas fa-phone me-3"></i> + 01 234 567 88</p>
+          <p><i class="fas fa-print me-3"></i> + 01 234 567 89</p>
+        </div>
+        <!-- Grid column -->
+      </div>
+      <!-- Grid row -->
+    </div>
+  </section>
+  <!-- Section: Links  -->
+
+  <!-- Copyright -->
+  <div class="text-center p-4" style="background-color: rgba(0, 0, 0, 0.05);">
+    © 2025 Copyright:
+    <a class="text-reset fw-bold" href="https://cocoschool.in/">Cocoschool.in</a>
+  </div>
+  <!-- Copyright -->
+</div>
+<!-- Footer -->
