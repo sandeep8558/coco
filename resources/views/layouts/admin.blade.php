4336b6c8ab70bcc74c017af4c10943785e69e1c0
sandeep8558 <sandeep198558@gmail.com>
2025-01-23 02:37:13 +0530

    Admin Sidebar design done

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -8,7 +8,58 @@
 </head>
 <body>
 
-@yield('content')
+<div class="">
+
+    <!-- Sidebar -->
+    <div id="sidebar" class="sidebar sidebar-open text-bg-light p-2">
+        
+        <ul class="list-group list-group-flush">
+            <li class="list-group-item p-0 bg-none mb-2" style="">
+                <a class="btn btn-primary shadow d-block text-start" href="/dashboard">Dashboard</a>
+            </li>
+            <li class="list-group-item p-0 bg-none mb-2">
+                <a class="btn btn-primary shadow d-block text-start" href="/">Website</a>
+            </li>
+            <li class="list-group-item p-0 bg-none mb-2">
+                <a class="btn btn-primary shadow d-block text-start" href="/dashboard">Grades</a>
+            </li>
+            <li class="list-group-item p-0 bg-none mb-2">
+                <a class="btn btn-primary shadow d-block text-start" href="/dashboard">Documents</a>
+            </li>
+            <li class="list-group-item p-0 bg-none mb-2">
+                <a class="btn btn-primary shadow d-block text-start" href="/dashboard">Grade Wise Documents</a>
+            </li>
+            <li class="list-group-item p-0 bg-none mb-2">
+                <a class="btn btn-primary shadow d-block text-start" href="/dashboard">Academic Year</a>
+            </li>
+            <li class="list-group-item p-0 bg-none mb-2">
+                <a class="btn btn-primary shadow d-block text-start" href="/dashboard">Admission Notice</a>
+            </li>
+            <li class="list-group-item p-0 bg-none mb-2">
+                <a class="btn btn-primary shadow d-block text-start" href="/dashboard">Application</a>
+            </li>
+            <li class="list-group-item p-0 bg-none mb-2">
+                <form method="POST" action="{{ route('logout') }}">
+                    @csrf
+                    <a class="btn btn-primary shadow d-block text-start" href="route('logout')" onclick="event.preventDefault(); this.closest('form').submit();">{{ __('Logout') }}</a>
+                </form>
+            </li>
+
+            
+        </ul>
+        
+    </div>
+
+    <!-- Content -->
+    <div id="sidebar_content" class="sidebar-content sidebar-content-open">
+        
+        <button id="sidebar-toggle">Open Close</button>
+
+        @yield('content')
+
+    </div>
+
+</div>
 
 </body>
 </html>
