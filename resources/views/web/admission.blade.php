c1d1bc4d2ca2ccf92f2567f7d7f8912c04e414e9
sandeep8558 <sandeep198558@gmail.com>
2025-01-24 03:20:32 +0530

    Admission Page Designed

--- resources/views/web/admission.blade.php
+++ resources/views/web/admission.blade.php
@@ -5,8 +5,23 @@
 @endsection
 
 @section('content')
-<h1>New Admission Open</h1>
-<h2>Leena</h2>
-<h3>Sandeep</h3>
-<h4>Hello</h4>
+    <div class="container py-4">
+        <h1>Admission</h1>
+    </div>
+
+    @foreach($admission_notices as $notice)
+    <div class="container shadow p-4 rounded">
+        <h4 class="mb-0">{{$notice->notification_title}}</h4>
+        <p class="mb-2">{{ $notice->academic_year->title }} ({{ $notice->academic_year->alias }})</p>
+        Online Application Date: {{ date('d-m-Y', strtotime($notice->start_date)) }} to {{ date('d-m-Y', strtotime($notice->end_date)) }} <br>
+        Application fee: Rs: {{ $notice->application_fee }}/-
+
+        @foreach($notice->admission_notice_grade_seats as $angs)
+        <div>{{ $angs->grade->grade }} - {{ $angs->seats }} seats</div>
+        @endforeach
+        <a class="btn btn-primary mt-2" href="/online_application/{{$notice->id}}">Apply Online</a>
+    </div>
+    @endforeach
+
+
 @endsection
